242d9f86210033ba9e1b91290e2c04c8cb6c83f5
krishkd16 <krishdihora16@gmail.com>
2023-09-23 01:54:07 +0530

    k

---
+++ app/src/main/java/com/krizyo/plantmed/activity/ProfileActivity.java
@@ -0,0 +1,40 @@
+package com.krizyo.plantmed.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.krizyo.plantmed.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    ImageView back;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        /*define id*/
+        toolbar=findViewById(R.id.toolbar);
+        back=findViewById(R.id.back);
+
+        /*activate toolbar*/
+        setSupportActionBar(toolbar);
+
+        /*      set on click listener on back button*/
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               /* Intent home=new Intent(BellActivity.this,MainActivity.class);
+                startActivity(home);*/
+                finish();
+
+            }
+        });
+    }
+}
